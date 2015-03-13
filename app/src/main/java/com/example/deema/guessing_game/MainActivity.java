1e7a4cac99d6fb9d87bce8493edff411e21c513d
didris <dimaid@hotmail.com>
2015-03-13 16:48:38 +0200

    some changes

--- app/src/main/java/com/example/deema/guessing_game/MainActivity.java
+++ app/src/main/java/com/example/deema/guessing_game/MainActivity.java
@@ -19,8 +19,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // TextView mytext=(TextView)findViewById(R.id.textView2);
-        //EditText edittext = (EditText)findViewById(R.id.edittext);
     }
 
 
